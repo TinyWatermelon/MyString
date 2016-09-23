b1d348d269de11c560c0eb96a0894b45fc5f0773
TinyWatermelon <louisleechn@gmail.com>
2016-09-23 21:14:01 +0800

    Revert "Update"
    This reverts commit 46f0752f356ec2b427fb9774eb213b7ab1236d70.

--- ClassString/Header.h
+++ ClassString/Header.h
@@ -1,39 +1,32 @@
-#include<iostream>
-using namespace std;
-class String
+#include<stdio.h>
+
+class string
 {
 public:
-	String(const char *str=NULL);
-	String(String&);
-	String(const int size);
-	~String();
-	void operator =(const String &);
-	char operator [](const int index);//
-	friend ostream& operator << (ostream& output,String& s); //ostream& output
-	friend istream& operator >> (istream& input, String& s);
-	void assign(const char *s);
-	void assign(const char *s, int n);
-	void assign(int n, char c);
-	void operator+=(const String &s);
-	void append(const char *s);
-	static int Compare(String& strA, String& strB);
-	static int Compare(String& strA, String& strB, bool ignoreCase);
-	int * Indexof(String& value);
-	int * Indexof(String& value, int startIndex);
-	int * Indexof(String& value, int startIndex, int count);
-	String replace(String oldValue, String newValue);
-	static String trim(String& value);
-	static String trim(String& value, String trimString);
-	static String trimStart(String& value, String trimString);
-	static String trimEnd(String& value, String trimString);
-	static int getLength(const char* value);
-	static int getLength(String& value);
-	static int getLength(String& value, String insertString);
-	static int getLength(String& value, String insertString, String trimString);
-	static void upperCase(String& value);//
-	int getTimes(String& value);//
-	int getTimes(String& value, int);
-	int getTimes(String& value, int, int);
+	string(const char *str=NULL);
+	string(string&);
+	~string();
+	void operator =(const string &);
+	string operator >> (const string &);
+	string operator <<(const string &);
+	string &assign(const char *s);
+	string &assign(const char *s, int n);
+	string &assign(int n, char c);
+	void operator+=(const string &s);
+	string &append(const char *s);
+	static int Compare(string strA, string strB);
+	static int Compare(string strA, string strB, bool ignoreCase);
+	static int Indexof(string value);
+	static int Indexof(string value, int startIndex);
+	static int Indexof(string value, int startIndex, int count);
+	static string replace(string oldValue, string newValue);
+	static string trim(string value);
+	static string trim(string value, string trimString);
+	static string trimStart(string value, string trimString);
+	static string trimEnd(string value, string trimString);
+	static string getLength(string value);
+	static string getLength(string value, string insertString);
+	static string getLength(string value, string insertString, string trimString);
 private:
 	char* str;
 };
--- ClassString/func.cpp
+++ ClassString/func.cpp
@@ -1,20 +1,17 @@
 #include"header.h"
-String::String(const char* Str)
+string::string(const char* Str)
 {
-	str = new char[String::getLength(Str)+1];
+	str = new char;
 	int i = 0;
 	while (Str!=NULL&&Str[i]!='\0')
+	{
 		str[i++] = Str[i];
+	}
 	str[i] = '\0';
 }
-String::String(const int size)
+string::string(string& s)
 {
-	str = new char[size];
-	str[0] = '\0';
-}
-String::String(String& s)
-{
-	str = new char[String::getLength(s)+1];
+	str = new char;
 	int i = 0;
 	while (s.str[i]!='\0')
 	{
@@ -22,321 +19,101 @@ String::String(String& s)
 	}
 	str[i] = '\0';
 }
-String::~String()
+string::~string()
 {
-	delete [] str;
+	delete str;
 }
-void String::operator = (const String & os)
+void string::operator = (const string & os)
 {
+	string s;
 	int i = 0;
-	delete[] str;
-	str = new char[String::getLength(os.str) + 1];
 	while (os.str[i] != '\0')
+	{
 		str[i++] = os.str[i];
+	}
 	str[i] = '\0';
+	
 }
-ostream& operator << (ostream& output, String& s)
+//string string::operator >>(const string & os)
+//{
+//
+//}
+//string string::operator <<(const string & os)
+//{
+//
+//}
+string & string::assign(const char *s)
 {
-	output << s.str;
-	return output;
-}
-istream& operator >> (istream& input, String& s)
-{
-	input >> s.str;
-	return input;
-}
-void String::assign(const char *s)
-{
+	string ns;
 	int i = 0;
 	while (s[i]!='\0')
 	{
-		str[i++] = s[i];
+		ns.str[i++] = s[i];
 	}
 	str[i] = '\0';
+	return ns;
 }
-void String::assign(const char *s,int n)
+//string & string::assign(const char *s,int n)
+//{
+//	for(int i=0;i<n;i++)
+//	{
+//		str[i]=s[i];
+//	}
+//	return ;
+//}
+string & string::assign(int n,char c)
 {
+	string ns;
 	for(int i=0;i<n;i++)
 	{
-		str[i]=s[i];
+		ns.str[i]=c;
 	}
+	return ns;
 }
-void String::assign(int n,char c)
+void string::operator+=(const string &s)
 {
-	int i = 0;
-	for(;i<n;i++)
-	{
-		str[i]=c;
-	}
-	str[i] = '\0';
-}
-void String::operator+=(const String &s)
-{
-	char* temp = new char[String::getLength(*this) + String::getLength(s.str) + 1];
 	int i = 0, n = 0;
 	while (str[i]!='\0')
 	{
-		temp[i++] = str[i];
+		i++;
 	}
-	delete[] str;
-	str = temp;
 	while (s.str[n] != '\0')
 	{
 		str[i++] = s.str[n++];
 	}
 	str[i] = '\0';
 }
-void String::append(const char *s)
-{
-	int i = 0, n = 0;
-	while (str[i] != '\0')
-	{
-		i++;
-	}
-	while (s[n] != '\0')
-	{
-		str[i++] = s[n++];
-	}
-	str[i] = '\0';
-}
-int String::Compare(String& strA, String& strB)
-{
-	int i = 0;
-	while (strA.str[i++] == strB.str[i])
-		if (strA.str[i] == '\0')
-			return 0;
-	return (int)strA.str[i] - (int)strB.str[i];
-}
-int String::Compare(String& strA, String& strB, bool ignoreCase)
-{
-	if (ignoreCase)
-	{
-		String NstrA = strA,NstrB = strB;
-		String::upperCase(NstrA);
-		String::upperCase(NstrB);
-		return String::Compare(NstrA, NstrB);
-	}
-	else
-	{
-		return String::Compare(strA, strB);
-	}
-}
-int* String::Indexof(String& value)
-{
-	int* index = new int[this->getTimes(value) + 1];
-	int i = 0, n = 0, l = 0;
-	while (str[i]!='\0')
-	{
-		while (str[i++]==value.str[n])
-			if (value.str[++n]=='\0')
-			{
-				index[l++] = i - n;
-				n = 0;
-				if (str[i] == '\0')
-					break;
-			}
-		n = 0;
-	}
-	index[l] = -1;
-	return index;
-}
-int* String::Indexof(String& value, int startIndex)
-{
-	int* index = new int[this->getTimes(value) + 1];
-	int i = startIndex, n = 0, l = 0;
-	while (str[i] != '\0')
-	{
-		while (str[i++] == value.str[n])
-			if (value.str[++n] == '\0')
-			{
-				index[l++] = i - n;
-				n = 0;
-				if (str[i] == '\0')
-					break;
-			}
-		n = 0;
-	}
-	index[l] = -1;
-	return index;
-}
-int* String::Indexof(String& value, int startIndex, int count)//
-{
-	int* index = new int[this->getTimes(value) + 1];
-	int i = startIndex, n = 0, l = 0;
-	while (str[i] != '\0')
-	{
-		while (str[i++] == value.str[n])
-			if (value.str[++n] == '\0')
-			{
-				index[l++] = i - n;
-				n = 0;
-				if (str[i] == '\0')
-					break;
-			}
-		n = 0;
-		if (i == count)
-			break;
-	}
-	index[l] = -1;
-	return index;
-}
-String String::replace(String oldValue, String newValue)
-{
-	
-	int* index = Indexof(oldValue);
-	int ovLength = String::getLength(newValue);
-	int nvLength = String::getLength(oldValue);
-	int times = getTimes(oldValue);
-	String Nstr(String::getLength(*this) - times*ovLength + times*nvLength + 1);
-	int i = 0, n = 0, l = 0;
-	while (str[i]!='\0')
-	{
-		if (i==index[l])
-		{
-			for (int a = 0; a < nvLength; a++)
-			{
-				Nstr.str[n++] = newValue[a];
-			}
-			i += ovLength;
-			l++;
-		}
-		Nstr.str[n++] = str[i++];
-	}
-	Nstr.str[n] = '\0';
-	delete [] index;
-	return Nstr;
-}
-String String::trim(String& value)
-{
-	int* index = value.Indexof((String)" ");
-	int i = 0, n = 0, l = 0, times = value.getTimes((String)" ");
-	String Nstr(String::getLength(value) - times + 1);
-	while (value.str[i] != '\0')
-	{
-		while (i == index[l])
-		{
-			i++;
-			l++;
-		}
-		Nstr.str[n++] = value.str[i++];
-	}
-	Nstr.str[n] = '\0';
-	delete [] index;
-	return Nstr;
-}
-String String::trim(String& value, String trimString)
-{
-	int* index = value.Indexof(trimString);
-	int i = 0, n = 0, l = 0, times = value.getTimes(trimString), trimStringLength = String::getLength(trimString);
-	String Nstr(String::getLength(value) - times*trimStringLength + 1);
-	while (value.str[i] != '\0')
-	{
-		while (i == index[l])
-		{
-			i += trimStringLength;
-			l++;
-		}
-		Nstr.str[n++] = value.str[i++];
-	}
-	Nstr.str[n] = '\0';
-	delete [] index;
-	return Nstr;
-}
-String String::trimStart(String& value, String trimString)
-{
-	int* index = value.Indexof(trimString);
-	int i = 0, n = 0, l = 0, times = value.getTimes(trimString), trimStringLength = String::getLength(trimString);
-	String Nstr(String::getLength(value) - times*2 + 1);
-	while (value.str[i] != '\0')
-	{
-		while (i == index[l])
-		{
-			i += 2;
-			l++;
-		}
-		Nstr.str[n++] = value.str[i++];
-	}
-	Nstr.str[n] = '\0';
-	delete [] index;
-	return Nstr;
-}
-String String::trimEnd(String& value, String trimString)
-{
-	int* index = value.Indexof(trimString); 
-	int i = 0, n = 0, l = 0, times = value.getTimes(trimString), trimStringLength = String::getLength(trimString);
-	String Nstr(String::getLength(value) - times * 2 + 1);
-	while (value.str[i] != '\0')
-	{
-		while (i == index[l])
-		{
-			for(int a=0;a<trimStringLength-2;a++)
-				Nstr.str[n++] = value.str[i++];
-			i += 2;
-			l++;
-		}
-		Nstr.str[n++] = value.str[i++];
-	}
-	Nstr.str[n] = '\0';
-	delete[] index;
-	return Nstr;
-}
-int String::getLength(const char* Str)
-{
-	if (Str == NULL)
-		return 0;
-	int i = 0;
-	while (Str[i++] != '\0');
-	return i - 1;
-}
-int String::getLength(String& value)
-{
-	int i = 0;
-	while (value.str[i++] != '\0');
-	return i - 1;
-}
-int String::getLength(String& value, String insertString)
-{
-	int i = 0;
-	while (value.str[i++] != '\0');
-	while (insertString.str[i++] != '\0');
-	return i - 2;
-}
-int String::getLength(String& value, String insertString, String trimString)
-{
-	int i = 0;
-	while (value.str[i++] != '\0');
-	while (insertString.str[i++] != '\0');
-	while (trimString.str[i++] != '\0');
-	return i - 3;
-}
-void String::upperCase(String& value)
-{
-	int i = 0;
-	while (value.str[i] != '\0')
-	{
-		if (value.str[i] > 96 && value.str[i] < 123)
-			value.str[i] -= 32;
-		i++;
-	}
-	//while (*str1++ = *dtr++);
-}
-char String::operator [](const int index)
-{
-	return str[index];
-}
-int String::getTimes(String& value)
-{
-	int i = -1, n = 0, l = 0;
-	while (str[i] != '\0')
-	{
-		while (str[i++] == value.str[n])
-			if (value.str[++n] == '\0')
-			{
-				n = 0;
-				l++;
-				if (str[i] == '\0')
-					break;
-			}
-	}
-	return l;
-}
+//string & string::append(const char *s)
+//{
+//
+//}
+//int string::Compare (string strA, string strB)
+//{
+//
+//}
+//int string::Compare(string strA, string strB, bool ignoreCase)
+//{
+//
+//}
+//int string::Indexof(string value)
+//{}
+//int string::Indexof(string value, int startIndex)
+//{}
+//int string::Indexof(string value, int startIndex, int count)
+//{}
+//string string::replace(string oldValue, string newValue)
+//{}
+//string string::trim(string value)
+//{}
+//string string::trim(string value, string trimString)
+//{}
+//string string::trimStart(string value, string trimString)
+//{}
+//string string::trimEnd(string value, string trimString)
+//{}
+//string string::getLength(string value)
+//{}
+//string string::getLength(string value, string insertString)
+//{}
+//string string::getLength(string value, string insertString, string trimString)
+//{}
